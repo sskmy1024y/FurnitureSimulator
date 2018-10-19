8c6e30705ef160d29f8ed53cd72984d48a460374
sho <>
2018-10-19 21:59:31 +0900

    template commit

---
+++ MEMO.md
@@ -0,0 +1,3 @@
+```
+npm -S i react react-dom webpack webpack-cli babel-loader @babel/core @babel/preset-env @babel/preset-react http-server
+```
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
